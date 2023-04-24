5f6cc6f556ab480b08d7557c4a81dfb3013b07c9
Jaroslav-89 <jaroslav89@bk.ru>
2023-04-24 12:01:31 +0400

    Добавил Activity и навигацию

---
+++ app/src/main/java/com/jar89/playlistmaker/AlbumsActivity.kt
@@ -0,0 +1,11 @@
+package com.jar89.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_albums)
+    }
+}
--- app/src/main/java/com/jar89/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/jar89/playlistmaker/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.jar89.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+        val albumsBtn = findViewById<Button>(R.id.albums_btn)
+        val settingsBtn = findViewById<Button>(R.id.settings_btn)
+
+        val searchButtonClickListener: View.OnClickListener = View.OnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        searchBtn.setOnClickListener(searchButtonClickListener)
+
+
+
+        albumsBtn.setOnClickListener {
+            val albumsIntent = Intent(this, AlbumsActivity::class.java)
+            startActivity(albumsIntent )
+        }
+
+        settingsBtn.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+
+
+
+
     }
 }
