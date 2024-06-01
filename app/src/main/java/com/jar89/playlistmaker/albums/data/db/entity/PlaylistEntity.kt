0d2e129dc8d0b99c85d2759b64bde5e7ba1b0bda
Jaroslav-89 <107784053+Jaroslav-89@users.noreply.github.com>
2024-06-01 14:41:13 +0400

    Спринт 23.2

--- app/src/main/java/com/jar89/playlistmaker/albums/data/db/entity/PlaylistEntity.kt
+++ app/src/main/java/com/jar89/playlistmaker/albums/data/db/entity/PlaylistEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "playlist_table")
-data class PlaylistEntity (
+data class PlaylistEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
